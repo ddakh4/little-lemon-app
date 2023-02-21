e8eb0d9bad78630bb9d3975a834f748650bfd01a
ddakh4 <ddakh4@gmail.com>
2023-02-21 11:01:19 +0200

    added unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import BookingForm from "./BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("BookingForm", () => {
+  const availableTimes = ["10:00 AM", "12:00 PM", "02:00 PM"];
+
+  it("should update the available times when the user selects a date", () => {
+    const updateTimes = jest.fn();
+    const { getByTestId } = render(
+      <BookingForm availableTimes={availableTimes} updateTimes={updateTimes} />
+    );
+    const dateInput = getByTestId("date");
+
+    fireEvent.change(dateInput, { target: { value: "2023-03-01" } });
+
+    expect(updateTimes).toHaveBeenCalledTimes(1);
+    expect(updateTimes).toHaveBeenCalledWith(new Date("2023-03-01"));
+  });
+
+  it("should submit the form when the user clicks on the submit button", () => {
+    const { getByTestId } = render(<BookingForm availableTimes={availableTimes} />);
+    const submitButton = getByTestId("submit");
+
+    fireEvent.click(submitButton);
+
+    expect(window.location.href).toContain("/confirmation");
+  });
 });
