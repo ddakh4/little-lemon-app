ba9ffd15b2ec99cfe99fb4e736f4b6aabb13dd07
ddakh4 <ddakh4@gmail.com>
2023-02-23 20:57:48 +0200

    added confirmed booking

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ConfirmedBooking() {
+    return (
+        <div className="confirmation-header">
+        <h1>Your reservation is confirmed!</h1>
+        <h4>We look forward to your visit and hope you will be 
+            enjoying your meal experience at Little Lemon as much as we will be enjoying your company.</h4>
+         </div>
+    )
+}
+
+export default ConfirmedBooking
