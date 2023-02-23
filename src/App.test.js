2c90a7aa9738a8216a24ca76296cebb4491475bf
ddakh4 <ddakh4@gmail.com>
2023-02-23 21:03:10 +0200

    added unit test for form input validation

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from "./BookingForm";
+import BookingPage from "./pages/Booking/BookingPage";
 
 describe("BookingForm", () => {
   const availableTimes = ["10:00 AM", "12:00 PM", "02:00 PM"];
@@ -26,4 +27,90 @@ describe("BookingForm", () => {
 
     expect(window.location.href).toContain("/confirmation");
   });
+});
+
+jest.mock('./fetchAPI', () => ({
+  __esModule: true,
+  default: jest.fn(() => Promise.resolve(['10:00', '11:00', '12:00'])),
+}));
+
+describe('BookingPage', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the booking heading and form', async () => {
+    await act(async () => {
+      render(<BookingPage />);
+    });
+
+    expect(screen.getByText(/booking/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /book/i })).toBeInTheDocument();
+  });
+
+  it('should fetch the available booking times', async () => {
+    await act(async () => {
+      render(<BookingPage />);
+    });
+
+    expect(fetchAPI).toHaveBeenCalledWith(new Date());
+  });
+
+  it('should display the available booking times', async () => {
+    await act(async () => {
+      render(<BookingPage />);
+    });
+
+    expect(screen.getByText(/10:00/i)).toBeInTheDocument();
+    expect(screen.getByText(/11:00/i)).toBeInTheDocument();
+    expect(screen.getByText(/12:00/i)).toBeInTheDocument();
+  });
+
+
+  describe('BookingForm', () => {
+    it('should show error messages for invalid inputs', async () => {
+      render(<BookingForm />);
+
+      // Find form fields
+      const dateInput = screen.getByTestId('date');
+      const timeInput = screen.getByTestId('time');
+      const guestsInput = screen.getByTestId('guests');
+      const occasionInput = screen.getByTestId('occasion');
+      const submitButton = screen.getByTestId('submit');
+
+      // Attempt to submit form with invalid inputs
+      fireEvent.change(dateInput, { target: { value: '' } });
+      fireEvent.change(timeInput, { target: { value: '' } });
+      fireEvent.change(guestsInput, { target: { value: '0' } });
+      fireEvent.change(occasionInput, { target: { value: '' } });
+      fireEvent.click(submitButton);
+
+      // Verify error messages are shown
+      const errorMessage = await screen.findAllByTestId('error');
+      expect(errorMessage).toHaveLength(4);
+    });
+
+    it('should submit form for valid inputs', async () => {
+      render(<BookingForm />);
+
+      // Find form fields
+      const dateInput = screen.getByTestId('date');
+      const timeInput = screen.getByTestId('time');
+      const guestsInput = screen.getByTestId('guests');
+      const occasionInput = screen.getByTestId('occasion');
+      const submitButton = screen.getByTestId('submit');
+
+      // Enter valid input values
+      fireEvent.change(dateInput, { target: { value: '2022-02-28' } });
+      fireEvent.change(timeInput, { target: { value: '7:00 PM' } });
+      fireEvent.change(guestsInput, { target: { value: '4' } });
+      fireEvent.change(occasionInput, { target: { value: 'Birthday' } });
+      fireEvent.click(submitButton);
+
+      // Verify that form was submitted successfully
+      const confirmationMessage = await screen.findByTestId('confirmation-message');
+      expect(confirmationMessage).toBeInTheDocument();
+    });
+  });
+
 });
