2732b0374e7abccbc71d55d5119eaffc2b9a3a74
ddakh4 <ddakh4@gmail.com>
2023-02-23 22:21:54 +0200

    updated App

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import HomePage from './pages/Home/HomePage';
 import BookingPage from './pages/Booking/BookingPage'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import ConfirmedBooking from './components/ConfirmedBooking';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    document.title = location.pathname === "/" ? "Home" : location.pathname;
-  }, [location]);
   return (
     <>
       <Nav />
