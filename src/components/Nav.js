552589308995c09ce6ecb44455f8eccf0c3cfc00
ddakh4 <ddakh4@gmail.com>
2023-02-24 10:30:58 +0200

    responsive navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import navLogo from "../assets/nav-logo.svg"
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
@@ -17,25 +17,48 @@ function Nav() {
   //   navbar.classList.remove("active");
   // };
 
+  // useEffect(() => {
+  //   const menu = document.querySelector('#menu-icon');
+  //   const navbar = document.querySelector('.navbar ul');
+
+  //   menu.addEventListener("click", function () {
+  //     navbar.classList.toggle("active");
+  //   });
+
+  //   window.onscroll = () => {
+  //     navbar.classList.remove("active");
+  //   };
+  // }, []);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
   useEffect(() => {
-    const menu = document.querySelector('#menu-icon');
-    const navbar = document.querySelector('.navbar ul');
-
-    menu.addEventListener("click", function () {
-      navbar.classList.toggle("active");
-    });
-
-    window.onscroll = () => {
-      navbar.classList.remove("active");
+    const handleMenuClick = () => {
+      setIsMenuActive(!isMenuActive);
+      console.log("icon clicked")
     };
-  }, []);
+
+    const handleWindowScroll = () => {
+      setIsMenuActive(false);
+      console.log("scrolled")
+    };
+
+    const menu = document.querySelector("#menu-icon");
+    const navbar = document.querySelector(".navbar");
+
+    menu.addEventListener("click", handleMenuClick);
+    window.addEventListener("scroll", handleWindowScroll);
+
+    return () => {
+      menu.removeEventListener("click", handleMenuClick);
+      window.removeEventListener("scroll", handleWindowScroll);
+    };
+  }, [isMenuActive]);
 
   return (
-    // <body>
       <header>
       <img src={navLogo} alt="Little Lemon Logo" className='logo'/>
       <div  class="bx bx-menu" id="menu-icon"></div>
-      <nav class="navbar">
+      <nav className={`navbar ${isMenuActive ? "active" : "ul"}`}>
         <ul>
         <li><Link className="a" to="/">Home</Link></li>
         <li><Link className="a" to="/">About</Link></li>
@@ -46,7 +69,6 @@ function Nav() {
         </ul>
         </nav>
       </header>
-    // </body>
   )
 }
 
